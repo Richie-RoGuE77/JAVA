4531dccc87dc1d45500d2142749c6adbb03c64b6
richie777 <79468696+Richie-RoGuE77@users.noreply.github.com>
2021-09-01 06:16:10 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,18 @@
+class Vehicle
+{            //method overriding
+void run(){System.out.println("Vehicle is running");
+}  
+}  
+class Bike extends Vehicle
+{  
+void run()
+{
+System.out.println("Bike is running safely");
+}  
+  
+public static void main(String args[])
+{  
+Bike obj = new Bike();
+obj.run();
+}  
+}  
